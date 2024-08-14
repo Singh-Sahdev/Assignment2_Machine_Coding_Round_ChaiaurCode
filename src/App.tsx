0c28932f06db5172a6e622b645987bd46d6a6cdc
Sahdev Singh <singhsahdev051@gmail.com>
2024-08-15 00:35:33 +0530

    Initialize the project and configured the Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface Props{
+  
+}
+
+const App:React.FC<Props> = function () {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
